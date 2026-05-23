963b4409bc2adc200c6558b6467ece5e31a4e172
Xohan Niloy <xohanniloy3399@gmail.com>
2026-05-23 16:29:31 +0600

    Bump plugin version and update compatibility tags

--- export-woocommerce.php
+++ export-woocommerce.php
@@ -3,12 +3,12 @@
 Plugin Name: Export Products, Orders & Customers for WooCommerce
 Plugin URI: https://wpfactory.com/item/export-woocommerce/
 Description: Advanced export tools for all your WooCommerce store data: Orders, Products Customers & More, export to XML or CSV in one click.
-Version: 2.3.3
+Version: 2.3.4
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: export-woocommerce
 Domain Path: /langs
-WC tested up to: 10.6
+WC tested up to: 10.7
 Requires Plugins: woocommerce
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -62,7 +62,7 @@ if ( 'export-woocommerce.php' === basename( __FILE__ ) ) {
 	}
 }
 
-defined( 'ALG_WC_EXPORT_VERSION' ) || define( 'ALG_WC_EXPORT_VERSION', '2.3.3' );
+defined( 'ALG_WC_EXPORT_VERSION' ) || define( 'ALG_WC_EXPORT_VERSION', '2.3.4' );
 
 defined( 'ALG_WC_EXPORT_FILE' ) || define( 'ALG_WC_EXPORT_FILE', __FILE__ );
 
